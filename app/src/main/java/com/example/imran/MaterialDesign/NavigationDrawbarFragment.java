f2d327a6923ded1e4727003630ebd0867580d6b8
imufun <imufun1@gmail.com>
2015-05-12 22:33:08 +0600

    navigation drawer

--- app/src/main/java/com/example/imran/MaterialDesign/NavigationDrawbarFragment.java
+++ app/src/main/java/com/example/imran/MaterialDesign/NavigationDrawbarFragment.java
@@ -1,6 +1,8 @@
 package com.example.imran.MaterialDesign;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -15,14 +17,29 @@ import com.example.imran.lolopomaterial.R;
 
 public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
 
+    public static final String FERE_FILE_NAME="testpref";
+    public static final String KEY_USER_LEARED_DRAWER="usser_lear_drawer";
 
     private ActionBarDrawerToggle mdrawerToggle;
     private DrawerLayout mDrawerLayout;
+
+    private boolean mUserlearnerDrawer;
+    private boolean mFormSaveInstanceState;
 
     public NavigationDrawbarFragment() {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUserlearnerDrawer=Boolean.valueOf(readFormPreferences(getActivity(),KEY_USER_LEARED_DRAWER,"false"));
+        if (savedInstanceState!=null){
+           mFormSaveInstanceState=true;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,8 +51,35 @@ public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
 
     public void setUp(DrawerLayout drawerLayout, Toolbar toolbar) {
         mDrawerLayout=drawerLayout;
-       mdrawerToggle=new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.draw_open,R.string.draw_close);
+       mdrawerToggle=new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.draw_open,R.string.draw_close){
 
+           @Override
+           public void onDrawerOpened(View drawerView) {
+               super.onDrawerOpened(drawerView);
+           }
+
+           @Override
+           public void onDrawerClosed(View drawerView) {
+               super.onDrawerClosed(drawerView);
+           }
+       };
+
+        mDrawerLayout.setDrawerListener(mdrawerToggle);
+
+    }
+
+    public void savetoPreferences(Context context,String preferenceName,String preferenceValue){
+
+        SharedPreferences sharedPreferences=context.getSharedPreferences(FERE_FILE_NAME,context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+
+        editor.putString(preferenceName,preferenceValue);
+        editor.apply();
+    }
+    public static String readFormPreferences(Context context,String preferenceName,String defealtValue){
+
+        SharedPreferences sharedPreferences=context.getSharedPreferences(FERE_FILE_NAME, context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName,defealtValue);
 
     }
 }
