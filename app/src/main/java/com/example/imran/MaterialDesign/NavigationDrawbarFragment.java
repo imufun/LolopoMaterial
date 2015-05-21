36ea80c17bb0a3b2697b26fef1831cb675df00a4
imufun <imufun1@gmail.com>
2015-05-21 07:46:31 +0600

    some new class added

--- app/src/main/java/com/example/imran/MaterialDesign/NavigationDrawbarFragment.java
+++ app/src/main/java/com/example/imran/MaterialDesign/NavigationDrawbarFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,9 @@ import android.view.ViewGroup;
 import android.support.v7.widget.Toolbar;
 
 import com.example.imran.lolopomaterial.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
@@ -30,6 +34,7 @@ public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
     private boolean mFormSaveInstanceState;
 
     public View ContainerView;
+    private ViewAdapter adapter;
 
 
     public NavigationDrawbarFragment() {
@@ -52,16 +57,41 @@ public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View layout=inflater.inflate(R.layout.fragment_navigation_drawbar, container, false);
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawbar, container, false);
 
-        recyclerView=(RecyclerView)layout.findViewById(R.id.drawList);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawList);
+
+        adapter = new ViewAdapter(getActivity(), getData()); // ViewAdapter class ke ekhane initialize korlam
+        recyclerView.setAdapter(adapter); // recyclerview e viewadapter initialize korlam
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())); // ekhane LinearLayoutManager,coz amar protiti item eker por asbe
         return layout;
     }
 
 
-    public void setUp(int fragmentId,DrawerLayout drawerLayout, final Toolbar toolbar) {
+    // getdata all item form custome_row
+    // eta dynamic hobe karon navigation e json,php,sql theke data niya jabe na
+    //
 
-        ContainerView=getActivity().findViewById(fragmentId);
+    public static List<Information> getData() {
+
+        List<Information> data = new ArrayList<>();
+        int[] icons = {R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher};// array nilam karon shob icon and tile show korar jonne
+        String[] titles = {"HTML", "CSS", "JAVA", "Android"};
+
+        for (int i = 0; i < titles.length && i < icons.length; i++) {
+            Information current = new Information();// object create korla ,shob value ke assagin korar jonne
+            current.iconId = icons[i]; // icons
+            current.title = titles[i]; // title
+            data.add(current);  // shob value ke add korlam
+        }
+        return data; // data return korlam ,List<Information> er maddhome
+
+    }
+
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+
+        ContainerView = getActivity().findViewById(fragmentId);
         mDrawerLayout = drawerLayout;
         mdrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.draw_open, R.string.draw_close) {
 
@@ -81,12 +111,13 @@ public class NavigationDrawbarFragment extends android.support.v4.app.Fragment {
                 super.onDrawerClosed(drawerView);
                 getActivity().invalidateOptionsMenu();
             }
+
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 super.onDrawerSlide(drawerView, slideOffset);
                 //Log.d("IMU","offset"+slideOffset);
-                if (slideOffset<0.6)
-                    toolbar.setAlpha(1-slideOffset);
+                if (slideOffset < 0.6)
+                    toolbar.setAlpha(1 - slideOffset);
 
             }
         };
---
+++ app/src/main/java/com/example/imran/MaterialDesign/ViewAdapter.java
@@ -0,0 +1,76 @@
+package com.example.imran.MaterialDesign;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.imran.lolopomaterial.R;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.Inflater;
+
+/**
+ * Created by imran on 5/15/15.
+ */
+public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    List<Information>data= Collections.emptyList(); //
+
+    public ViewAdapter(Context context,List<Information>data) {
+        inflater=LayoutInflater.from(context);
+
+        this.data=data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.coustome_row,parent,false);// ekhane custome row call kortrese
+        MyViewHolder holder=new MyViewHolder(view);//
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Information current=data.get(position);//call Information class
+
+        holder.title.setText(current.title);//information ---tile initialize
+
+        holder.icon.setImageResource(current.iconId);//Information--icon initialize
+
+    }
+
+
+    //- shob itam call hobe ei method er maddhome --getItemCount (android.support.v7.widget);
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    //Custome_row call hoise ekhane
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        // textview and icon call here
+
+        TextView title;
+        ImageView icon;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            title=(TextView)itemView.findViewById(R.id.TextTile);
+            icon=(ImageView)itemView.findViewById(R.id.ImgIcon);
+        }
+    }
+
+
+
+}
